fe53ed467134a580ad169cba13eb3ec0d2202730
denissokolov <denis.sokolov@saltpepper.ru>
2022-11-12 22:23:05 +0300

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import path from "path";
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "components": path.resolve(__dirname, "src/components"),
+      "pages": path.resolve(__dirname, "src/pages"),
+      "constants": path.resolve(__dirname, "src/constants"),
+      "utils": path.resolve(__dirname, "src/utils"),
+      "styles": path.resolve(__dirname, "src/styles"),
+      "hooks": path.resolve(__dirname, "src/hooks"),
+      "types": path.resolve(__dirname, "src/types")
+    },
+  },
+})
