08b2bf33eb09ccec4583b8cf224ff4a9a579e1ec
denissokolov <denis.sokolov@saltpepper.ru>
2022-11-12 22:51:03 +0300

    add: constants for routes

---
+++ src/components/Routers/routes.ts
@@ -0,0 +1,10 @@
+import {IRouter} from "components/Routers/types";
+import Home from "pages/Home";
+
+export const routes: IRouter[] = [
+  {
+    path: '/',
+    title: 'Главная',
+    component: Home
+  }
+]
