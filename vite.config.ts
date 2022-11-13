f5112ec2497faedeedccd73c9254dc33f92c42dc
denissokolov <denis.sokolov@saltpepper.ru>
2022-11-13 16:13:39 +0300

    add: axios, paths - api

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import path from "path";
+import path from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
@@ -7,13 +7,14 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "components": path.resolve(__dirname, "src/components"),
-      "pages": path.resolve(__dirname, "src/pages"),
-      "constants": path.resolve(__dirname, "src/constants"),
-      "utils": path.resolve(__dirname, "src/utils"),
-      "styles": path.resolve(__dirname, "src/styles"),
-      "hooks": path.resolve(__dirname, "src/hooks"),
-      "types": path.resolve(__dirname, "src/types")
+      components: path.resolve(__dirname, 'src/components'),
+      pages: path.resolve(__dirname, 'src/pages'),
+      constants: path.resolve(__dirname, 'src/constants'),
+      utils: path.resolve(__dirname, 'src/utils'),
+      styles: path.resolve(__dirname, 'src/styles'),
+      hooks: path.resolve(__dirname, 'src/hooks'),
+      types: path.resolve(__dirname, 'src/types'),
+      api: path.resolve(__dirname, 'src/api'),
     },
   },
 })
